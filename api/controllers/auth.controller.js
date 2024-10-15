00b6b57430b62a83b09f093c4bf848b1506f89d0
Sindhu4250 <si1056@unh.edu>
2024-10-15 00:48:40 -0400

    Add sign up page functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -5,6 +5,9 @@ import { errorHandler } from '../utils/error.js';
 export const signup = async(req, res, next) => {
     const{ username, email, password } = req.body;
 
+    
+
+
     if (!username ||
         !email || 
         !password ||
@@ -15,7 +18,7 @@ export const signup = async(req, res, next) => {
       next(errorHandler(400, 'All fields are required'));
 
     }
-
+    
     const hashedPassword = bcryptjs.hashSync(password, 10);
 
     const newUser = new User({ 
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,15 +1,52 @@
 import React from 'react';
-import { Button, Label, TextInput } from 'flowbite-react';
-import { Link } from 'react-router-dom';
+import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 export default function Signup() {
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setFormData({...formData, [e.target.id]: e.target.value.trim() });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData.username || !formData.email || !formData.password) {
+      return setErrorMessage('Please fill out all fields.');
+    }
+    try {
+      setLoading(true);
+      setErrorMessage(null);
+      const res = await fetch('/api/auth/signup', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        return setErrorMessage(data.message);
+      }
+      setLoading(false);
+      if(res.ok) {
+        navigate('/sign-in');
+      }
+    } catch (error) {
+      setErrorMessage(error.message);
+      setLoading(false);
+
+    }
+  };
+
+ 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
       {/* Welcome Text */}
       <h1 className="text-2xl font-bold text-gray-700 mb-8">Hello, Welcome to Blog Canvas!</h1>
 
       {/* Form container */}
-      <form className="bg-white p-6 rounded-lg shadow-lg w-full max-w-md">
+      <form className="bg-white p-6 rounded-lg shadow-lg w-full max-w-md" onSubmit={handleSubmit}>
         {/* Username Field */}
         <div className="mb-4">
           <Label htmlFor="username" value="Your Username" className="text-gray-700" />
@@ -18,7 +55,8 @@ export default function Signup() {
             type="text"
             placeholder="Enter your username"
             className="mt-2"
-            required={true}
+            onChange={handleChange}
+            
           />
         </div>
 
@@ -30,7 +68,8 @@ export default function Signup() {
             type="email"
             placeholder="Enter your email"
             className="mt-2"
-            required={true}
+            onChange={handleChange}
+            
           />
         </div>
 
@@ -42,24 +81,40 @@ export default function Signup() {
             type="password"
             placeholder="Enter your password"
             className="mt-2"
-            required={true}
+            onChange={handleChange}          
+            
           />
         </div>
+      
 
         {/* Signup Button */}
-        <Button type="submit" className="w-full bg-purple-700 text-white hover:bg-purple-800">
-          Sign Up
+        <Button type="submit" className="w-full bg-purple-700 text-white hover:bg-purple-800" disabled={loading}>
+        {loading ? (
+                <>
+                  <Spinner size='sm' />
+                  <span className='pl-3'>Loading...</span>
+                </>
+              ) : (
+                'Sign Up'
+              )}
         </Button>
 
          {/* Already have an account? */}
          <div className="mt-4 text-center">
           <p className="text-gray-600">
             Already have an account?{' '}
-            <Link to="/sign-in" className="text-purple-700 hover:underline">
+            <Link to="/sign-in" className="text-orange-500 hover:underline">
               Sign In
             </Link>
           </p>
         </div>
+        {errorMessage && (
+          <Alert className="mt-5" color="failure">
+            {errorMessage}
+          </Alert>
+        )}
+
+        
 
         
       </form>
